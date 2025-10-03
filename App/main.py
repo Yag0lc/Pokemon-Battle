5e8a382bb139c731089e5be1030acb41548efbb4
Hugo0806 <hugo.reyzas@WIRTZ.LOCAL>
2025-10-03 17:03:55 +0200

    fase 1 hasta requisito 2

--- App/main.py
+++ App/main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, json, jsonify
+from flask import current_app
 
 app = Flask(__name__)
 
@@ -8,3 +9,18 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 8080)
+
+
+with open("data/pokemon.json", encoding="utf-8") as f:
+    app.config["DATA"] = json.load(f)
+
+
+
+from flask import current_app
+
+# Carga de datos del fichero
+...
+
+@app.route("/")
+def home():
+    return jsonify(current_app.config["data"])
